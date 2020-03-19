23e720a205946d31fe8355e215313e3659a9fe8f
bryantran3562 <bryantran3562@gmail.com>
2020-03-19 13:37:07 -0500

    step2: Nested components

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,29 @@
 <template>
 <!-- BT - This is a ROOT component file - where you would need to nest all of your other components -->
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <!-- BT - Nested component:
-              Step3: Add your nested component here.
-              This is your nested component called - HelloWorld.vue  -->
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- BT - Step4 - Nested Locally:
+        a. Finally, add your new component here.
+    -->
+    <Ninja></Ninja>
   </div>
 </template>
 
 <script>
-// BT - Nested component:
-//Step1: import your nested component here including the path.
-import HelloWorld from './components/HelloWorld.vue'
+// BT - Step2 - Nested Locally:
+//a. Import your new component here with import statement
+
+import Ninja from '@/components/Ninja';
 
 export default {
   name: 'App',
-  //BT - Nested component:
-  //Step2: Include your nested component here.
+  //BT - Step3 - Nested Locally:
+  //a. Include your nested component here.
   components: {
-    HelloWorld
+    Ninja
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Ninja.vue
@@ -0,0 +1,44 @@
+<template>
+<!-- BT - All of your new component consisted of 3 main part:
+          a. <template></template> This is where your html part is at
+          b. <script></script> This is where you can control the data from here to your template html
+          c. <style></style> This is where you can do your style css
+ -->
+ <!-- BT - You can only have a single <div></div> in your template. All other must be nested inside this root <div></div> -->
+
+ <!-- BT - Nested component - There are two ways that you can do this:
+           a. Nested locally.
+           b. Nested globally.
+ -->
+ <!-- BT - Step1 - Nested Globally:
+           a. Create you new component.
+           b. Go to main.js and register your new component there
+ -->
+  <!-- BT - Step1 - Nested Locally:
+           a. Create you new component.
+           b. Go to App.vue(Root component file) and register your new component there
+ -->
+  <div>
+    <h1>{{ name }}</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+  data(){
+
+    return {
+        name: 'HelloWorld',
+    }
+  }
+
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
