3a74d64f1b020d72e68f5ce5ff1801c620942274
bryantran3562 <bryantran3562@gmail.com>
2020-03-19 09:48:28 -0500

    Step1

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+<!-- BT - This is a ROOT component file - where you would need to nest all of your other components -->
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- BT - Nested component:
+              Step3: Add your nested component here.
+              This is your nested component called - HelloWorld.vue  -->
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// BT - Nested component:
+//Step1: import your nested component here including the path.
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  //BT - Nested component:
+  //Step2: Include your nested component here.
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
