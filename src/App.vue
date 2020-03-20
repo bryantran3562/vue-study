891e70b49f1d1819fdee816217174a3bbe113ae7
bryantran3562 <bryantran3562@gmail.com>
2020-03-20 17:57:23 -0500

    step8-Dynamic-component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
 <!-- BT - This is a ROOT component file - where you would need to nest all of your other components -->
   <div id="app">
-    <!-- BT - Step4 - Nested Locally:
-        a. Finally, add your new component here.
+
+    <!-- <Ninja></Ninja>
+    <Ninja2></Ninja2> -->
+
+    <!-- BT - Step 1: Just need to use the component directive and bind it to the dynamic variable.
+                      Remember, to make a dynamic variable, we must declared it in the data() section,
     -->
-    <Ninja></Ninja>
+    <component v-bind:is="component"></component>
+
+    <!-- BT - These are dynamicly to load a right components -->
+    <button @click="component = 'Ninja'">
+      change component 1
+    </button>
+
+    <button @click="component = 'Ninja2'">
+      change component 2
+    </button>
+
   </div>
 </template>
 
 <script>
-// BT - Step2 - Nested Locally:
-//a. Import your new component here with import statement
+//##############################################################################################
+// BT - Dynamic Component: The idea is that - We want to dynamicly to display a different
+//                         component based on user input.
+//###############################################################################################
 
 import Ninja from '@/components/Ninja';
+import Ninja2 from '@/components/Ninja-2';
 
 export default {
   name: 'App',
-  //BT - Step3 - Nested Locally:
-  //a. Include your nested component here.
+
   components: {
-    Ninja
-  }
+    Ninja,
+    Ninja2,
+  },
+  data(){
+    return {
+      component: 'Ninja'
+    }
+  },
+
 }
 </script>
 
--- src/components/Ninja-2.vue
+++ src/components/Ninja-2.vue
@@ -18,7 +18,7 @@ export default {
   data(){
 
     return {
-        name: 'Ninja 1',
+        name: 'Ninja 2',
     }
   }
 
