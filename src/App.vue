95854601291a06833fafc22e6962205c9918445f
bryantran3562 <bryantran3562@gmail.com>
2020-03-20 20:27:52 -0500

    blogs-http-get

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
 <!-- BT - This is a ROOT component file - where you would need to nest all of your other components -->
   <div id="app">
 
-    <addBlog></addBlog>
+    <!-- <addBlog></addBlog> -->
+    <showBlogs></showBlogs>
  
   </div>
 </template>
@@ -11,14 +12,16 @@
 // BT - Step2 - Nested Locally:
 //a. Import your new component here with import statement
 
-import addBlog from '@/components/addBlog';
+//import addBlog from '@/components/addBlog';
+import showBlogs from './components/showBlogs.vue';
 
 export default {
   name: 'App',
   //BT - Step3 - Nested Locally:
   //a. Include your nested component here.
   components: {
-    addBlog
+    //addBlog,
+    showBlogs
   }
 }
 </script>
